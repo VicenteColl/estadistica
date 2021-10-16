c4deced1ca327d415c9b8cc9481e9b21d46a1462
VicenteColl <vicencoll@gmail.com>
2021-10-16 12:14:27 +0200

    version 4

---
+++ R/distribucion.normal.R
@@ -0,0 +1,31 @@
+#' @title Distribución normal.
+#'
+#' @description Aplicación interactiva para comparar dos distribuciones normales.
+#' @usage distribucion.normal()
+#'
+#' @author
+#' \strong{Vicente Coll-Serrano} (\email{vicente.coll@@uv.es}).
+#' \emph{Métodos Cuantitativos para la Medición de la Cultura (MC2). Economía Aplicada.}
+#'
+#' \strong{Olga Blasco-Blasco} (\email{olga.blasco@@uv.es}).
+#' \emph{Métodos Cuantitativos para la Medición de la Cultura (MC2). Economía Aplicada.}
+#'
+#' \strong{Rosario Martínez Verdú} (\email{rosario.martinez@@uv.es}).
+#' \emph{Economía Aplicada.}
+#'
+#' \strong{Cristina Pardo García} (\email{cristina.pardo-garcia@@uv.es}).
+#' \emph{Métodos Cuantitativos para la Medición de la Cultura (MC2). Economía Aplicada.}
+#'
+#' Facultad de Economía. Universidad de Valencia (España)
+#'
+#' @import shiny
+#'
+#' @export
+distribucion.normal <- function() {
+  appDir <- system.file("examples/normal", package = "estadistica")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
