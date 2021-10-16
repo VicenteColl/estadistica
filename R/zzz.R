7ee30a60acb52ab89b4916abd5f4d83f921af09c
VicenteColl <vicencoll@gmail.com>
2021-10-16 12:35:16 +0200

    version 4

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   packageStartupMessage("Este paquete est\u00e1 pensado para servir de apoyo a la docencia de estad\u00edstica descriptiva e inferencial.")
-  packageStartupMessage("Este paquete est\u00e1 en desarrollo. Por favor, si detectas errores  o quieres hacernos alguna sugeriencia, puedes ponerte en contacto con en la direcci\u00f3n de correo:")
+  packageStartupMessage("Este paquete est\u00e1 en desarrollo. Por favor, si detectas errores  o quieres hacernos alguna sugeriencia, puedes ponerte en contacto con nosotros en:")
   packageStartupMessage("vicencoll@gmail.com")
 }
 
